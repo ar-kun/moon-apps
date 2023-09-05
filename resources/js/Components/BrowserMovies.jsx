9e593e8309bfcd9657fdae6018a913b7e36a795d
Adam Ramdan <arshinks12@gmail.com>
2023-09-05 15:23:41 +0700

    feat: integration detail movie

--- resources/js/Components/BrowserMovies.jsx
+++ resources/js/Components/BrowserMovies.jsx
@@ -36,7 +36,7 @@ export default function BrowserMovies({ slug, name, category, thumbnail }) {
                 />
             </div>
             <Link
-                href={route("prototype.movie.show", slug)}
+                href={route("user.dashboard.movie.show", slug)}
                 className="inset-0 absolute z-50"
             ></Link>
         </div>
